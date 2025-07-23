42194bf19ed7d8373255f27b1e50ce7852a01aeb
ArshidaKA <arshidaakbar39@gmail.com>
2025-07-23 22:12:36 +0530

    Update Userlayout.jsx

--- src/Layout/Userlayout.jsx
+++ src/Layout/Userlayout.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Outlet, useLocation } from "react-router-dom";
+import WhatsappButton from "../components/WhatsAppButton";
 
 function Userlayout() {
   const location = useLocation();
